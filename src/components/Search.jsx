ef7c59b282e64184c6897298d2691416f56a2051
sntwandy <sntwandy@gmail.com>
2020-06-18 14:46:05 -0400

    Search component and theirs styles

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styles
+import '../assets/styles/components/Search.scss';
+
+const Search = () => {
+    return(
+        <section className="main">
+            <h2 className="main__title">¿Qué quieres ver hoy?</h2>
+            <input type="text" class="input" placeholder="Buscar..." />
+        </section>
+    );
+};
+
+export default Search;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,12 +5,13 @@ import '../assets/styles/App.scss';
 
 // Components
 import Header from '../components/Header';
+import Search from '../components/Search';
 
 const App = () => {
     return(
         <React.Fragment>
             <Header />
-
+            <Search />
         </React.Fragment>
     );
 };
