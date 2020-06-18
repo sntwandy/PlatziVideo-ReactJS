ed5446a9a3f7598fff24d5857163b605a9f86c0d
sntwandy <sntwandy@gmail.com>
2020-06-18 14:31:34 -0400

    Creating our container App

--- src/components/HelloWorld.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import '../assets/styles/App.scss';
-
-const HelloWorld = () => (
-      <h1>Te amo Megan E.</h1>
-);
-
-export default HelloWorld;
---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Components
+import Header from '../components/Header';
+
+const App = () => {
+    return(
+        <React.Fragment>
+            <Header />
+
+        </React.Fragment>
+    );
+};
+
+export default App;
