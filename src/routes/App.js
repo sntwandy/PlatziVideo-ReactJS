f4ffa0033eb4a37bee6584cc7d191c0494e8c822
sntwandy <sntwandy@gmail.com>
2020-06-25 18:49:35 -0400

    Adding Login container

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 // Components
 import Home from '../containers/Home';
+import Login from '../containers/Login';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
         </BrowserRouter>
     );
 };
