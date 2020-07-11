78e0c96633e5b0410c1374a6d0f9bdbeed3e4128
sntwandy <sntwandy@gmail.com>
2020-07-11 15:21:02 -0400

    Finishing to config the Server to show the Frontend (Server Side Rendering)

--- src/server/index.js
+++ src/server/index.js
@@ -2,4 +2,6 @@ require('@babel/register')({
   presets: ['@babel/preset-env','@babel/preset-react']
 });
 
+require('ignore-styles');
+
 require('./server');
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { renderRoutes } from 'react-router-config'
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { StaticRouter } from 'react-router-dom';
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
+import serverRoutes from '../frontend/routes/serverRoutes';
 
 dotenv.config();
 
@@ -23,8 +32,8 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 };
 
-app.get('*', (req, res) => {
-  res.send(`
+const setResponse = (html) => {
+  return (`
       <!DOCTYPE html>
       <html lang="en">
       <head>
@@ -34,12 +43,27 @@ app.get('*', (req, res) => {
           <title>Platzi Video | App</title>
       </head>
       <body>
-          <div id="app"></div>
+          <div id="app">${html}</div>
           <script src="assets/app.js" type="text/javascript"></script>
       </body>
       </html>
   `);
-});
+}
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  );
+
+  res.send(setResponse(html));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
