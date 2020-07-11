6da55939f05d858e73880ab654fa695ab0fe1dd3
sntwandy <sntwandy@gmail.com>
2020-07-11 15:49:29 -0400

    Loading resources  statics from the server

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,11 @@
+require('ignore-styles');
 require('@babel/register')({
   presets: ['@babel/preset-env','@babel/preset-react']
 });
 
-require('ignore-styles');
+require('asset-require-hook')({
+  extensions: ['jpg', 'png', 'gif'],
+  name: '/assets/[hash].[ext]',
+});
 
 require('./server');
--- src/server/server.js
+++ src/server/server.js
@@ -10,6 +10,7 @@ import { StaticRouter } from 'react-router-dom';
 import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState';
 import serverRoutes from '../frontend/routes/serverRoutes';
+import Layout from '../frontend/components/Layout';
 
 dotenv.config();
 
@@ -55,7 +56,9 @@ const renderApp = (req, res) => {
   const html = renderToString(
     <Provider store={store}>
       <StaticRouter location={req.url} context={{}}>
-        {renderRoutes(serverRoutes)}
+        <Layout>
+          {renderRoutes(serverRoutes)}
+        </Layout>
       </StaticRouter>
     </Provider>
   );
